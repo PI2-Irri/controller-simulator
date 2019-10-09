01799cf9e72d300eb19ddfd5df5d35fb0974b0a1
Guilherme Augusto <guilherme.francais@gmail.com>
2019-10-08 21:13:18 -0300

    Configures compose structure to run all project stack

--- simulator/settings.py
+++ simulator/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-j6#+=gl)950ku4%f%q^58&o7_$26destzr((en&6*hp1gogt^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv('ENVIRONMENT') == 'production':
+    DEBUG = False
+else:
+    DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

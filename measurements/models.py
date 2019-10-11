c0c53649d5afdbe30cc1dbff29d622e9d2020ee9
Guilherme Augusto <guilherme.francais@gmail.com>
2019-10-11 17:05:03 -0300

    Fixes type and name measurements attributes

--- measurements/models.py
+++ measurements/models.py
@@ -6,11 +6,11 @@ from modules.models import Module
 class ActuatorMeasurement(models.Model):
     token = models.CharField(max_length=30, default=os.getenv('CENTRAL_TOKEN'))
     water_consumption = models.FloatField(default=0.0)
-    water_level = models.IntegerField(default=0)
+    reservoir_level = models.IntegerField(default=0)
     battery_level = models.IntegerField(default=0)
 
 
 class ModuleMeasurement(models.Model):
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
     temperature = models.FloatField(default=0.0)
-    humidity = models.FloatField(default=0.0)
+    ground_humidity = models.IntegerField(default=0.0)
